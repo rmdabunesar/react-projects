35da0dbcde4a100b94b6a915e6374a84eadef9ef
rmdabunesar <abunesar13@gmail.com>
2025-03-17 12:22:29 +0600

    connect with repository

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Accordian from "./components/accordian/Accordian";
+
+const App = () => {
+  return (
+    <div>
+      <Accordian />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/accordian/Accordian.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Accordian = () => {
+    return (
+        <div>
+            <h1>Hello Accordian</h1>
+        </div>
+    );
+};
+
+export default Accordian;
