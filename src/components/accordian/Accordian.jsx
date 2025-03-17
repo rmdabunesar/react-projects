14107ba47af2ce0bf46b8f2e1b14214fa5c03d79
rmdabunesar <abunesar13@gmail.com>
2025-03-17 13:36:01 +0600

    accordian

--- src/components/accordian/Accordian.jsx
+++ src/components/accordian/Accordian.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+// single selection accordian
+// multiple selection accordian
+
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
 
 const Accordian = () => {
-    return (
-        <div>
-            <h1>Hello Accordian</h1>
-        </div>
-    );
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let copyMutiple = [...multiple];
+    const findIndexOfCurrentId = copyMutiple.indexOf(getCurrentId);
+
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) copyMutiple.push(getCurrentId);
+    else copyMutiple.splice(findIndexOfCurrentId, 1);
+
+    setMultiple(copyMutiple);
+  }
+
+  return (
+    <div className="acc-wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="acc-content ">{dataItem.answer}</div>
+                  )}
+            </div>
+          ))
+        ) : (
+          <div>No data found !</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Accordian;
