af5bee92d641a411ce789f35ef34eea9255f8906
rmdabunesar <abunesar13@gmail.com>
2025-03-24 12:00:08 +0600

    star rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Accordian from "./components/accordian/Accordian";
 import RandomColor from "./components/random-color/RandomColor";
+import StarRating from "./components/star-rating/StarRating";
 
 const App = () => {
   return (
     <div>
       {/* <Accordian /> */}
 
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      <StarRating noOfStars={5} />
     </div>
   );
 };
